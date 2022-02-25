4ccbe8041342e6fc7e32af3fce6f523050e30732
Bas Lepelaar <6010805@mborijnland.nl>
2022-02-25 11:24:13 +0100

    .

--- HoBo/routes/web.php
+++ HoBo/routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/profiel', function () {
+    return view('profiel');
+})->middleware(['auth'])->name('profiel');
+
 require __DIR__.'/auth.php';
