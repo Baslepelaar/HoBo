db45461284dd8973b273b850a09cd6825083b97a
fewpixels <74591245+fewpixels@users.noreply.github.com>
2022-03-21 09:54:01 +0100

    edited bootstrap menu
    alpha

--- HoBo/index.php
+++ HoBo/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'partial/header2.php';
+require_once 'partial/header.php';
 ?>
     <main>
       <section class="content">
--- HoBo/partial/header.php
+++ HoBo/partial/header.php
@@ -7,7 +7,7 @@
       <meta name="author" content="Maikel Braas">
       <meta name="keywords" content="html,css-- mijn eigen website">
       <title>Ra site</title>
-      <link rel="stylesheet" href="css/global2.css">
+      <link rel="stylesheet" href="css/global.css">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
@@ -25,17 +25,17 @@
             </a>
           </li>
           <li>
-            <a href="#" class="nav-link" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
+            <a href="#" class="nav-link bg-secondary" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
             <div><img src="img/pop512.png" style="height: 2rem;"></div>
             </a>
           </li>
           <li>
-            <a href="#" class="nav-link" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Orders">
+            <a href="#" class="nav-link bg-secondary" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Orders">
             <div><img src="img/vergroot512.png" style="height: 2rem;"></div>
             </a>
           </li>
           <li>
-            <a href="#" class="nav-link" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Products">
+            <a href="#" class="nav-link bg-secondary" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Products">
             <div><img src="img/tijd512.png" style="height: 2rem; justify-content: center;"></div>
             </a>
           </li>
