330c31cfd6d537b030c1f0cf679abfb2503f3cda
MathieuvanBeek <74538092+MathieuvanBeek@users.noreply.github.com>
2022-03-21 09:02:38 +0100

    Footer aangepast

--- HoBo/partial/footer.php
+++ HoBo/partial/footer.php
@@ -1,38 +1,47 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<footer class="bg-light text-center text-lg-start">
+  <footer class="text-center text-lg-start">
+  <div class="text-center p-4 d-flex justify-content-center justify-content-lg-around" style="background-color: #7da599;">
+  <p class="socials" style="font-size: 2rem; color: black;">Socials:</p>
+  <a href="#!" class="bi-facebook" style="font-size: 2rem; color: black;">
+  <a href="#!" class="bi-instagram" style="font-size: 2rem; color: black;"></a>
+  <a href="#!" class="bi-youtube" style="font-size: 2rem; color: black;"></a>
+  <a href="#!" class="bi-linkedin" style="font-size: 2rem; color: black;"></a>
+  <a href="#!" class="bi-github" style="font-size: 2rem; color: black;"></a>
+  </div>
   <div class="container p-4">
     <div class="row">
       <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
         <h5 class="text-uppercase">Filmslijst</h5>
-        <ul class="list-unstyled mb-0">
-          <li>
-            <a href="#!" class="text-dark">Link 1</a>
+        <ul class="list-unstyled mb-0 ">
+          <li class="text-decoration-none">
+            <a href="#!" class="text-decoration-none text-dark">Alle films</a>
           </li>
           <li>
-            <a href="#!" class="text-dark">Link 2</a>
+            <a href="#!" class="text-decoration-none text-dark">Genre's</a>
           </li>
           <li>
-            <a href="#!" class="text-dark">Link 3</a>
+            <a href="#!" class="text-decoration-none text-dark">Beoordelingen</a>
           </li>
           <li>
-            <a href="#!" class="text-dark">Link 4</a>
+            <a href="#!" class="text-decoration-none text-dark">Link 4</a>
           </li>
         </ul>
       </div>
       <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase mb-0">Pagina's</h5>
-        <ul class="list-unstyled">
+        <h5 class="text-uppercase">Pagina's</h5>
+        <ul class="list-unstyled mb-0">
           <li>
-            <a href="#!" class="text-dark">Link 1</a>
+            <a href="#!" class="text-decoration-none text-dark">Link 1</a>
           </li>
           <li>
-            <a href="#!" class="text-dark">Link 2</a>
+            <a href="#!" class="text-decoration-none text-dark">Link 2</a>
           </li>
           <li>
-            <a href="#!" class="text-dark">Link 3</a>
+            <a href="#!" class="text-decoration-none text-dark">Contact</a>
           </li>
           <li>
-            <a href="#!" class="text-dark">Link 4</a>
+            <a href="#!" class="text-decoration-none text-dark">Link 4</a>
           </li>
         </ul>
       </div>
@@ -40,40 +49,40 @@
         <h5 class="text-uppercase">Over ons</h5>
         <ul class="list-unstyled mb-0">
           <li>
-            <a href="#!" class="text-dark">Link 1</a>
+            <a href="#!" class="text-decoration-none text-dark">Link 1</a>
           </li>
           <li>
-            <a href="#!" class="text-dark">Link 2</a>
+            <a href="#!" class="text-decoration-none text-dark">Link 2</a>
           </li>
           <li>
-            <a href="#!" class="text-dark">Link 3</a>
+            <a href="#!" class="text-decoration-none text-dark">Link 3</a>
           </li>
           <li>
-            <a href="#!" class="text-dark">Link 4</a>
+            <a href="#!" class="text-decoration-none text-dark">Link 4</a>
           </li>
         </ul>
       </div>
       <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase mb-0">Contact</h5>
-        <ul class="list-unstyled">
+        <h5 class="font-weight-bold text-uppercase">Contact</h5>
+        <ul class="list-unstyled mb-0">
           <li>
-            <a href="#!" class="text-dark">Link 1</a>
+            <a href="#!" class="text-decoration-none text-dark">Email</a>
           </li>
           <li>
-            <a href="#!" class="text-dark">Link 2</a>
+            <a href="#!" class="text-decoration-none text-dark">Telefoon</a>
           </li>
           <li>
-            <a href="#!" class="text-dark">Link 3</a>
+            <a href="#!" class="text-decoration-none text-dark">Adres</a>
           </li>
           <li>
-            <a href="#!" class="text-dark">Link 4</a>
+            <a href="#!" class="text-decoration-none text-dark">Link 4</a>
           </li>
         </ul>
       </div>
     </div>
   </div>
-  <div class="text-center p-3" style="background-color: #375f00;">
-    © 2022 Copyright: Hobo.nl
+  <div class="text-center p-4" style="background-color: #276955;">
+<b>© 2022 Copyright: Hobo.nl</b>
   </div>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
