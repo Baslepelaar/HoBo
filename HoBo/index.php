a9c8cff9961dee95357f3cdc7b113951c968ac13
MathieuvanBeek <74538092+MathieuvanBeek@users.noreply.github.com>
2022-03-21 11:23:33 +0100

    Home pagina aangepast

--- HoBo/index.php
+++ HoBo/index.php
@@ -1,25 +1,25 @@
 <?php
 require_once 'partial/header.php';
 ?>
+
+<body>
     <main>
-      <section class="content">
-        <article class="info">
-          <p>dit is wat text</p>
-        </article>
-        <article class="info">
-          
-        </article>
-      </section>
-      <section class="content">
-        <article class="info">
-          
-        </article>
-        <article class="info">
-          
-        </article>
-      </section>
+        <section class="row w-100 h-100 align-items-center">
+            <div class="container-fluid">
+                <article class="col-md-6 background"><img src="img/SW-banner.png" alt="Star Wars banner"></article>
+                <article class="col-md-6 logo card-img-overlay"><img src="img/SW-logo.png" alt="Star Wars logo" style="height: 5vw;"></article>
+                <article class="col-md-4 card-img-overlay filmtekst">
+                    <p style="font-size: 1.5rem; color: grey;">Star Wars is een Amerikaanse epische space-opera-filmserie bedacht door George Lucas
+                    en ontwikkeld door zijn bedrijf Lucasfilm. Het epos bestaat uit drie afgeronde trilogieën,
+                    twee losstaande films, twee live-actionseries, meerdere animatieseries en meerdere stripreeksen.
+                    </p>
+                </article>
+               </div>
+        </section>  
+        <div class="bg-danger.bg-gradient">.bg-warning</div>
     </main>
+</body>
 
 <?php
 require_once 'partial/footer.php';
-?>
+?>  
