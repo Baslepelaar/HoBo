d8235369bd15ef6438220a20d1d992b3972b72ea
MathieuvanBeek <74538092+MathieuvanBeek@users.noreply.github.com>
2022-03-21 11:39:22 +0100

    Home pagina background image toegevoegt

--- HoBo/index.php
+++ HoBo/index.php
@@ -4,10 +4,10 @@ require_once 'partial/header.php';
 
 <body>
     <main>
+    <img class="bg-img" src="img/SW-banner.png" alt="Starwars banner">
         <section class="row w-100 h-100 align-items-center">
             <div class="container-fluid">
-                <article class="col-md-6 background"><img src="img/SW-banner.png" alt="Star Wars banner"></article>
-                <article class="col-md-6 logo card-img-overlay"><img src="img/SW-logo.png" alt="Star Wars logo" style="height: 5vw;"></article>
+                <article class="col-md-6 logo card-img-overlay"><img src="img/SW-logo.png" alt="Star Wars logo" style="height: 10vw;"></article>
                 <article class="col-md-4 card-img-overlay filmtekst">
                     <p style="font-size: 1.5rem; color: grey;">Star Wars is een Amerikaanse epische space-opera-filmserie bedacht door George Lucas
                     en ontwikkeld door zijn bedrijf Lucasfilm. Het epos bestaat uit drie afgeronde trilogieën,
@@ -16,7 +16,6 @@ require_once 'partial/header.php';
                 </article>
                </div>
         </section>  
-        <div class="bg-danger.bg-gradient">.bg-warning</div>
     </main>
 </body>
 
