d5a98440cfaaddc6e3ca564924dd05e56e0b7998
fewpixels <74591245+fewpixels@users.noreply.github.com>
2022-04-04 09:21:47 +0200

    ignoring main fork files

--- HoBo/login.php
+++ HoBo/login.php
@@ -1,62 +1,21 @@
 <?php
-    require_once 'partial/header.php';
-    require_once 'backend/class/Login.php';
-
-    $is_online = new Online();
-
-    $online = $is_online->getIs_online();
-    $banned = false;
-    if($online) {
-        $banned = true;
-    }
-    if($banned) {
-        header('Location: index.php');
-    }
-
-    $ip = new IP();
-
-    $getip = $ip->get_client_ip();
-
-    $ip->addIPtoList($getip);
-    if($ip->isIPBanned($getip)) {
-        header('Location: https://google.com');
-    }
-
-    $userIns = new Login();
-
-    if(isset($_POST['login'])){
-        $userIns->login($_POST);
-    }
-
+require_once 'partial/header.php';
 	
 ?>
 
-    <?php include('backend/alert.php'); ?>
     <main>
-		<section class="row w-100 h-100">
-        <article class="col-md-5 forum mt-5 p-4" style="background-color: green; border-radius: 1vw;">
-          <h2 class="h1-responsive font-weight-bold text-center my-4" style="color: white;">Inloggen</h2>
-              <div class="row">
-                <div class="col-md-9 mb-md-0 w-responsive mx-auto mb-5" style="margin-left: 5vw;">
-                  <form id="contact-form" name="contact-form" method="POST">
-                 <div class="row">
-              <div class="col-md-12">
-                <div class="md-form mt-3">
-                  <input type="email" id="name" name="email" class="form-control mb-3" placeholder="Email" required>
-                  <input type="password" id="email" name="password" class="form-control mb-3" placeholder="Password" required>
-                </div>
-              </div>
-            </form>
-              <div class="text-center text-md-left col-md-12">
-			  <input class="btn btn-primary"type="submit"  name="login" value="Login" style="background-color: #02ee5a; border-color: #02ee5a;">
-			  <a href="registratie.php" class="btn btn-primary" style="background-color: #02ee5a; border-color: #02ee5a;" onclick="">Registreren</a>
-			</div>
-			<div class="status"></div>
-              </div>
-            </div>
-          </section>
-      </article>
+    	<section class="form">
+	    	<form method="post">
+	    		<label for="username" id="username">Gebruikersnaam: </label>
+	    		<input type="text" name="username" required>
+	    		<label for="password">Wachtwoord: </label>
+	    		<input type="password" name="password" required>
+	    		<input type="submit" name="login" value="Login">
+	    	</form>
+	    	<a href="registratie.php">Registreren</a>
+    	</section>
     </main>
+
 
 <?php
 require_once 'partial/footer.php';
--- HoBo/partial/footer.php
+++ HoBo/partial/footer.php
@@ -1,89 +1,22 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <footer class="text-center text-lg-start">
-  <div class="text-center p-4 d-flex justify-content-center justify-content-lg-around" style="background-color: #0c0c0c;">
-  <p class="socials" style="font-size: 2rem; color: grey;">Socials:</p>
-  <a href="#!" class="bi-facebook" style="font-size: 2rem; color: grey;">
-  <a href="#!" class="bi-instagram" style="font-size: 2rem; color: grey;"></a>
-  <a href="#!" class="bi-youtube" style="font-size: 2rem; color: grey;"></a>
-  <a href="#!" class="bi-linkedin" style="font-size: 2rem; color: grey;"></a>
-  <a href="#!" class="bi-github" style="font-size: 2rem; color: grey;"></a>
-  </div>
-  <div class="container p-4">
-    <div class="row">
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase" style="font-size: 1.5rem; color: grey;">Filmslijst</h5>
-        <ul class="list-unstyled mb-0 ">
-          <li class="text-decoration-none">
-            <a href="#!" class="text-decoration-none" style="font-size: 1rem; color: grey;">Alle films</a>
-          </li>
-          <li>
-           <a href="#!" class="text-decoration-none" style="font-size: 1rem; color: grey;">Genre's</a>
-          </li>
-          <li>
-            <a href="#!" class="text-decoration-none" style="font-size: 1rem; color: grey;">Beoordelingen</a>
-          </li>
-          <li>
-           <a href="#!" class="text-decoration-none" style="font-size: 1rem; color: grey;">Link 4</a>
-          </li>
-        </ul>
-      </div>
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase" style="font-size: 1.5rem; color: grey;">Pagina's</h5>
-        <ul class="list-unstyled mb-0">
-          <li>
-            <a href="#!" class="text-decoration-none" style="font-size: 1rem; color: grey;">Link 1</a>
-          </li>
-          <li>
-            <a href="#!" class="text-decoration-none" style="font-size: 1rem; color: grey;">Link 2</a>
-          </li>
-          <li>
-            <a href="#!" class="text-decoration-none" style="font-size: 1rem; color: grey;">Contact</a>
-          </li>
-          <li>
-            <a href="#!" class="text-decoration-none" style="font-size: 1rem; color: grey;">Link 4</a>
-          </li>
-        </ul>
-      </div>
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase" style="font-size: 1.5rem; color: grey;">Over ons</h5>
-        <ul class="list-unstyled mb-0">
-          <li>
-          <a href="#!" class="text-decoration-none" style="font-size: 1rem; color: grey;">Link 1</a>
-          </li>
-          <li>
-          <a href="#!" class="text-decoration-none" style="font-size: 1rem; color: grey;">Link 2</a>
-          </li>
-          <li>
-          <a href="#!" class="text-decoration-none" style="font-size: 1rem; color: grey;">Link 3</a>
-          </li>
-          <li>
-          <a href="#!" class="text-decoration-none" style="font-size: 1rem; color: grey;">Link 4</a>
-          </li>
-        </ul>
-      </div>
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase" style="font-size: 1.5rem; color: grey;">Contact</h5>
-        <ul class="list-unstyled mb-0">
-          <li>
-          <a href="contact.php" class="text-decoration-none" style="font-size: 1rem; color: grey;">Email</a>
-          </li>
-          <li>
-          <a href="#!" class="text-decoration-none" style="font-size: 1rem; color: grey;">Adres</a>
-          </li>
-          <li>
-          <a href="overons.php" class="text-decoration-none" style="font-size: 1rem; color: grey;">Overons</a>
-          </li>
-          <li>
-          <a href="Login.php" class="text-decoration-none" style="font-size: 1rem; color: grey;">Login</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div class="text-center p-4" style="background-color: #0c0c0c;">
-<b style="color: grey;">© 2022 Copyright: Hobo.nl</b>
-  </div>
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+
+    <footer>
+      <article class="footer-content">
+        <h1>Site</h1>
+        <a href="login.php">Login</a>
+      </article>
+      <article class="footer-content">
+        <h1>Partners</h1>
+        <a href="login.php">Login</a>
+      </article>
+      <article class="footer-content">
+        <h1>Support</h1>
+        <a href="login.php">Login</a>
+      </article>
+      <article class="footer-content">
+        <h1>FAQ</h1>
+        <a href="login.php">Login</a>
+      </article>
+    </footer>
+  </body>
+</html>
