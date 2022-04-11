71f56d0b5002b6e9b6871d86dd38a16077cdff5f
fewpixels <74591245+fewpixels@users.noreply.github.com>
2022-04-11 09:55:50 +0200

    merged menu
    added working menu buttons
    
    this branch serves as a backup from main in case main gets corrupted

--- HoBo/partial/header.php
+++ HoBo/partial/header.php
@@ -7,43 +7,38 @@
       <meta name="author" content="Bas, William, Mathieu">
       <meta name="keywords" content="Hobo, streaming website, streaming">
       <title>Hobo</title>
-      <link rel="stylesheet" href="css/global.css">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+      <link rel="stylesheet" href="css/global.css">
   </head>
   <body>
+  <img src="img/logo.png" class="logoHeader">
     <header>
-      <img src="img/logo.png" class="logoHeader">
-      <div class="d-flex flex-column flex-shrink-0 align-items-center" >
-        <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
-          <li class="nav-item">
-            <a href="#" class="nav-link active" aria-current="page" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
-            <div class="align-items-center"><img src="img/home.png" style="height: 2rem;"></div>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="nav-link bg-secondary" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
-            <div><img src="img/pop512.png" style="height: 2rem;"></div>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="nav-link bg-secondary" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Orders">
-            <div><img src="img/vergroot512.png" style="height: 2rem;"></div>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="nav-link bg-secondary" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Products">
-            <div><img src="img/tijd512.png" style="height: 2rem; justify-content: center;"></div>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="nav-link" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Customers">
-              <svg class="bi" width="24" height="24" role="img" aria-label="Customers"><use xlink:href="#people-circle"></use></svg>
-            </a>
-          </li>
-        </ul>
-      </div>
+      <div class="d-flex flex-shrink-0 align-items-center" >
+          <ul class="nav nav-pills mb-auto text-center">
+            <li class="nav-item btn-sm">
+              <a href="index.php" class="nav-link active" aria-current="page" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
+              <div class="align-items-center"><img src="img/home.png" style="height: 2rem;"></div>
+              </a>
+            </li>
+            <li class="btn-sm">
+              <a href="login.php" class="nav-link bg-secondary" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
+              <div><img src="img/pop512.png" style="height: 2rem;"></div>
+              </a>
+            </li>
+            <li class="btn-sm">
+              <a href="overons.php" class="nav-link bg-secondary" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Orders">
+              <div><img src="img/vergroot512.png" style="height: 2rem;"></div>
+              </a>
+            </li>
+            <li class="btn-sm">
+              <a href="summary.php" class="nav-link bg-secondary" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Products">
+              <div><img src="img/tijd512.png" style="height: 2rem; justify-content: center;"></div>
+              </a>
+            </li>
+          </ul>
+        </div>
 
     </header>
---
+++ HoBo/stream.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'partial/header.php';
+?>
+    <main>
+        <div class="videostream ratio ratio-16x9">
+            <iframe
+              class="embed-responsive-item"
+              src="https://www.youtube.com/embed/x0E39ug6t70" 
+              title="YouTube video player" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+              allowfullscreen>
+            </iframe>
+        </div>
+    </main>
+
+<?php
+require_once 'partial/footer.php';
+?>
