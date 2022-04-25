5e7fcc3afbfaa32cd2f312f8f93c5cbd30d24931
MathieuvanBeek <74538092+MathieuvanBeek@users.noreply.github.com>
2022-04-25 10:33:16 +0200

    Contact, registratie en login pagina beter gestyled

--- HoBo/registratie.php
+++ HoBo/registratie.php
@@ -32,22 +32,22 @@
     }
 
 ?>
-
-    <?php include('backend/alert.php'); ?>
-
-    <main>
+<?php
+include('backend/alert.php'); 
+?>
+<main>
   <div class="row w-100 h-100 justify-content-center mt-5">
   <form id="contact-form" name="contact-form" method="POST">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col">
-        <div class="card card-registration my-4" style="border-radius: 2vh; border: white solid 0px">
+        <div class="card card-registration my-4" style="border-radius: 2vh; border: black solid 0px;">
           <div class="row g-0">
             <div class="col-xl-6 d-none d-xl-block">
               <img
-                src="img/image 1.jpg"
+                src="img/logo full 2.png"
                 alt="star wars achtergrond"
                 class="img-fluid"
-                style="border-top-left-radius: .25rem; border-bottom-left-radius: .25rem; height: 80vh; border-top-left-radius: 2vh; border-bottom-left-radius: 2vh;"/>
+                style="border-top-left-radius: .25rem; border-bottom-left-radius: .25rem; height: 80vh; border-top-left-radius: 2vh; border-bottom-left-radius: 2vh; background-color: black;"/>
             </div>
             <div class="col-xl-6">
               <div class="card-body p-md-5 text-black">
@@ -56,12 +56,12 @@
                 <div class="row">
                   <div class="col-md-6 mb-4">
                     <div class="form-outline">
-                    <input class="text-center mb-1" type="text" name="voornaam" style="height: 5vh; border-radius: 1vh;" placeholder="Voornaam" required>
+                    <input class="text-center mb-1" type="text" name="voornaam" style="height: 5vh; border-radius: 1vh; border: 2px solid grey;" placeholder="Voornaam" required>
                     </div>
                   </div>
                   <div class="col-md-6 mb-4">
                     <div class="form-outline">
-                    <input class="text-center mb-1" type="text" name="tussenvoegsel" class="text-center" style="height: 5vh; border-radius: 1vh;" placeholder="Tussenvoegsel">
+                    <input class="text-center mb-1" type="text" name="tussenvoegsel" class="text-center" style="height: 5vh; border-radius: 1vh; border: 2px solid grey;" placeholder="Tussenvoegsel">
                     </div>
                   </div>
                 </div>
@@ -69,23 +69,25 @@
                 <div class="row">
                   <div class="col-md-6 mb-4">
                     <div class="form-outline">
-                    <input class="text-center mb-1" type="text" name="achternaam" class="text-center" style="height: 5vh; border-radius: 1vh;" placeholder="Achternaam" required>
+                    <input class="text-center mb-1" type="text" name="achternaam" class="text-center" style="height: 5vh; border-radius: 1vh; border: 2px solid grey;" placeholder="Achternaam" required>
                     </div>
                   </div>
-
-                <div class="form-outline mb-4">
-                <input class="text-center mb-1" type="email" name="email" class="text-center"style="height: 5vh; border-radius: 1vh;"  placeholder="Email" required>
+                  <div class="col-md-6 mb-4">
+                <div class="form-outline">
+                <input class="text-center mb-1" type="email" name="email" class="text-center"style="height: 5vh; border-radius: 1vh; border: 2px solid grey;"  placeholder="Email" required>
                 </div>
-
+              </div>
+              </div>
+              
                 <div class="row">
                   <div class="col-md-6 mb-4">
                     <div class="form-outline">
-                    <input class="text-center mb-1" type="password" name="password" class="text-center" style="height: 5vh; border-radius: 1vh;" placeholder="Wachtwoord" required>
+                      <input class="text-center mb-1" type="text" name="password" style="height: 5vh; border-radius: 1vh; border: 2px solid grey;" placeholder="Wachtwoord" required>
                     </div>
                   </div>
                   <div class="col-md-6 mb-4">
                     <div class="form-outline">
-                    <input class="text-center mb-1" type="password" name="conf-password" class="text-center" style="height: 5vh; border-radius: 1vh;" placeholder="Wachtwoord bevestigen" required>
+                      <input class="text-center mb-1" type="text" name="password-conf" class="text-center" style="height: 5vh; border-radius: 1vh; border: 2px solid grey;" placeholder="Wachtwoord bevestigen">
                     </div>
                   </div>
                 </div>
@@ -99,12 +101,9 @@
                   <div class="col-md-12 mb-4">
                     <p>Heb je al een account? Klik dan <a class="text-decoration-none" href="login.php" style="color: black;"><b>Hier</a></p>
                   </div>
-
                   <div class="submit">
                     <input type="submit" class="btn btn-primary btn-lg" style="background-color: #02ee5a; border-color: #02ee5a; height: 5vh;" name="register" value="Register">
                 </div>
-
-              </div>
             </div>
           </div>
         </div>
@@ -112,8 +111,7 @@
     </div>  
     </form> 
   </div>
-</section>
-    </main>
+</main>
 
 <?php
     require_once 'partial/footer.php';
