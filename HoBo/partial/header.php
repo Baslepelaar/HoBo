1203d2c57c3b311ae99698fef6a3b02a3b6ecc16
Bas Lepelaar <74591239+Baslepelaar@users.noreply.github.com>
2022-05-07 14:17:53 +0200

    Maintenance

--- HoBo/partial/header.php
+++ HoBo/partial/header.php
@@ -1,12 +1,17 @@
 <?php
 session_start();
+//maintenance check
+require_once 'backend/class/Maintenance.php';
+
+$maintenance = new Maintenance();
+if($maintenance->getMaintenance() == '1') {
+    header('Location: maintenance.php');
+}
 require_once 'backend/class/UserRight.php';
-
-$userright = new UserRight();
-
-
 require_once 'backend/class/Online.php';
 require_once 'backend/class/IP.php';
+
+$userright = new UserRight();
 
 $id = '';
 
@@ -45,6 +50,7 @@ if($ip->isIPBanned($getip)) {
       <meta name="author" content="Bas, William, Mathieu">
       <meta name="keywords" content="Hobo, streaming website, streaming">
       <title>Hobo</title>
+      <link rel="icon" type="image/png" href="img/Logo.png" />
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
